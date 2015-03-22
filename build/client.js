3e35920df9e8c2400bb80a7bda30f729a790d862
mix irving <whimful@gmail.com>
2015-03-23 01:03:18 +1300

    tweak format

--- build/client.js
+++ build/client.js
@@ -2066,13 +2066,27 @@ xhr({
   var results = JSON.parse(body)
 
   if (results.error) { console.error(results.error) }
-  var content = '<div>' + results.lines.join('<br>').replace(/[0-9]{5,}|DCI/g,'').replace(/\s/g, '&nbsp;') + '</div>'
-   
+
+  var content = '<div>' +
+                  results.lines.map( lineFixer ).join('<br />')
+                '</div>'
+
   dom('#data-box').add( content )
 })
 
+function lineFixer(string) {
+  console.log(string)
+  return string
+         .replace(/(Round [0-9]+)/,"<strong>$1</strong><br><br>")
+         .replace(/[0-9]{5,}|DCI/g,'')
+         .replace(/\s/g, '&nbsp;')
+         .replace(/Wizards.*LLC/,'')
+         .replace(/Wizards.*(\d{4}\/\d{2}\/\d{2}.*)/, "$1")
+         .replace(/(Report|Event).*/, '')
+}
+
 document.querySelector('input').onchange = function() {
   document.querySelector('form').submit();
-};
+}
 
 },{"domquery":1,"xhr":39}]},{},[46]);
